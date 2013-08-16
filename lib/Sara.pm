650f59a960453ba8da9dcbb69b27883898ef0820
hirata-motoi <hirata.motoi@gmail.com>
2013-08-16 20:22:03 +0900

    initial commit

---
+++ lib/Sara.pm
@@ -0,0 +1,32 @@
+package Sara;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Sara -
+
+=head1 SYNOPSIS
+
+  use Sara;
+
+=head1 DESCRIPTION
+
+Sara is
+
+=head1 AUTHOR
+
+hirata.motoi E<lt>hirata.motoi@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
